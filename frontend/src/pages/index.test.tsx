6d4a979d221c41d210a54f579e2179b22672d50b
Joselia C <joselialcosta@gmail.com>
2023-04-26 13:31:03 +0200

    test: mocks useData and adds a test to onSubmit form

--- frontend/src/pages/index.test.tsx
+++ frontend/src/pages/index.test.tsx
@@ -1,17 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import Home from ".";
 
-describe("Home", () => {
-  global.fetch = jest.fn(() =>
-    Promise.resolve({
-      json: () => Promise.resolve(["babbel.com", "google.com", "linkedin.com"]),
-    })
-  ) as jest.Mock;
+jest.mock("../services/data", () => ({
+  useData: jest.fn(() => {
+    return ["facebook.com", "linkedin.com"];
+  }),
+}));
 
+describe("Home", () => {
   it("should render the form with 2 inputs and a submit button", () => {
     render(<Home />);
+
     expect(screen.getByText("Full name")).toBeInTheDocument();
     expect(screen.getByText("Company domain")).toBeInTheDocument();
     expect(screen.getByText("Search")).toBeInTheDocument();
@@ -27,6 +28,19 @@ describe("Home", () => {
       await userEvent.click(searchButton);
 
       expect(fullNameInput).toBeInvalid();
+    });
+
+    it("should not submit form if user not selected one option on company domains dropdown", async () => {
+      render(<Home />);
+      const fullNameInput = screen.getByRole("textbox");
+      const companyDomainInput = screen.getByRole("combobox");
+      const searchButton = screen.getByRole("button");
+      await act(async () => {
+        await userEvent.type(fullNameInput, "John Due");
+        await userEvent.click(searchButton);
+      });
+
+      expect(companyDomainInput).toBeInvalid();
     });
   });
 });
