5e8f9f8b2e049d2d976b41df3735e9525d42cc64
Joselia C <joselialcosta@gmail.com>
2023-04-26 14:59:47 +0200

    test: enhances mocks and integration test for onSubmit

--- frontend/src/pages/index.test.tsx
+++ frontend/src/pages/index.test.tsx
@@ -2,11 +2,11 @@ import { act, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import Home from ".";
+import { useSubmitForm } from "@/services/data";
 
 jest.mock("../services/data", () => ({
-  useData: jest.fn(() => {
-    return ["facebook.com", "linkedin.com"];
-  }),
+  useData: jest.fn(() => ({ data: ["google.com", "linkedin.com"] })),
+  useSubmitForm: jest.fn(),
 }));
 
 describe("Home", () => {
@@ -41,6 +41,35 @@ describe("Home", () => {
       });
 
       expect(companyDomainInput).toBeInvalid();
+    });
+
+    it("should submit form if user has all data valid", async () => {
+      render(<Home />);
+
+      // mock the return value of useSubmitForm
+      const mockResult = { data: "johndoe@linkedin.com" };
+      const mockUseSubmitForm = useSubmitForm as jest.MockedFunction<
+        typeof useSubmitForm
+      >;
+      mockUseSubmitForm.mockResolvedValue(mockResult);
+
+      const fullNameInput = screen.getByRole("textbox");
+      const companyDomainInput = screen.getByRole("combobox");
+      const searchButton = screen.getByRole("button");
+
+      await act(async () => {
+        await userEvent.type(fullNameInput, "John Due");
+        await userEvent.selectOptions(companyDomainInput, "linkedin.com");
+        await userEvent.click(searchButton);
+      });
+
+      expect(fullNameInput).toBeValid();
+      expect(companyDomainInput).toBeValid();
+      expect(mockUseSubmitForm).toHaveBeenCalledTimes(1);
+      expect(mockUseSubmitForm).toHaveBeenCalledWith({
+        fullName: "John Due",
+        companyDomain: "linkedin.com",
+      });
     });
   });
 });
