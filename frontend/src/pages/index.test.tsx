0b728d29ecbc362512836b7c87444b15f6f7f7a4
Joselia C <joselialcosta@gmail.com>
2023-04-26 08:23:25 +0200

    test: full name input validation

--- frontend/src/pages/index.test.tsx
+++ frontend/src/pages/index.test.tsx
@@ -16,4 +16,17 @@ describe("Home", () => {
     expect(screen.getByText("Company domain")).toBeInTheDocument();
     expect(screen.getByText("Search")).toBeInTheDocument();
   });
+
+  describe("Form", () => {
+    it("should not submit form if full name not contain at least 1 surname", async () => {
+      render(<Home />);
+      const fullNameInput = screen.getByRole("textbox");
+      const searchButton = screen.getByRole("button");
+
+      await userEvent.type(fullNameInput, "John");
+      await userEvent.click(searchButton);
+
+      expect(fullNameInput).toBeInvalid();
+    });
+  });
 });
